0bad822f3208bdc0b97139d5ef298f6ec77d475e
Roshawn Brooks <69084474+rbrooks95@users.noreply.github.com>
2022-03-18 21:45:47 -0400

    Add files via upload

--- src/dropDown.js
+++ src/dropDown.js
@@ -1,22 +1,34 @@
 import axios from "axios";
-import React from "react";
-import Axios from "axios";
+import React, { useState } from "react";
 
 function DropDown() {
+  const [job, setJob] = useState("");
+
   const fetchData = async () => {
     const response = await axios.get(
       "https://frontend-take-home.fetchrewards.com/form"
     );
-    console.log(response);
+
+    for (let jobs of response.data.occupations) {
+      console.log(jobs);
+      return jobs;
+    }
   };
   fetchData();
-
   return (
     <form id="form">
       <div className="form-group">
         <label>Occupation</label>
-        <select defaultValue placeholder="pick" required name="occupation">
-          <option defaultValue disabled selected hidden></option>
+        <select
+          placeholder="pick"
+          required
+          name="occupation"
+          onChange={() => {
+            const choice = this.response.data.occupations;
+            setJob(choice);
+          }}
+        >
+          <option value={fetchData}> {fetchData}</option>
         </select>
       </div>
     </form>
